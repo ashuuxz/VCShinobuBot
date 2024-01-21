95602d7976028d84559cbaaf0e4637b05f504f95
ashuuxz <145094756+ashuuxz@users.noreply.github.com>
2024-01-21 14:11:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Mandatory VARS
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-STRING_SESSION = getenv("STRING_SESSION")
-VIDEO_CHAT_ID = int(getenv("VIDEO_CHAT_ID"))
+API_ID = int(getenv("20906663"))
+API_HASH = getenv("f5b105d21d47fc193cccc3f308ec9f10")
+BOT_TOKEN = getenv("6964153477:AAGKBSUhaskQTRL2YBwfs7tCUstvxUJ_jCU")
+STRING_SESSION = getenv("BQBAQZIhJvtq_ZRffRxuOpqofaK9B3bvg_TsiXbi_8f-V7pxWTPMEKQJTy3cc8EGIoqPHus22ShswdFGeTaVzK9b-RyCSQZLCILtYVD3taunnNulb5sLRybR2PxPZ7n0uaOrXmeUd27nQ0XGx-Pjl4JpAMuMhU0Uvl3wuvUYyo5TAGSpVX-u0KLb6wzKsCjUyfrhSmP6Dptcvr_B6nBqo-USfh4Ed9hARxgvYIU1MrlKpm0YbDd2tA1zKLundCSMBHFw-cRtcgLeyztRmvP5GRwy2zzswpksM50z1SPS_fFppeUdGRdk9BLruhGOEz5rLtbxQlQrkP5wVZLwSHTf3ds7AAAAAX6txTEA")
+VIDEO_CHAT_ID = int(getenv("6420284721"))
